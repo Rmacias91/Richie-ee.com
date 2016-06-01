79690e5ed193f32b0361fd35310894000d1b4ec3
Me 240396 <240396@richieee.nfshost.com>
2016-06-01 15:08:10 +0000

    Able to read table on db with php

--- php/config.php
+++ php/config.php
@@ -1,16 +1,20 @@
 <?php
 $servername = "mydb.db";
 $username = "rmacias91";
-$password = "Richard10!";
+$password = "password";
+$dbname = "myTable";
 
 // Create connection
 $conn = mysql_connect($servername, $username, $password);
+
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-else{
-echo "Connected successfully";
-}
+
+$dbSelected= mysql_select_db($dbname,$conn);
+if(!$dbSelected){
+ die ('blog not selected : ' . mysql_error());  
+}  
 ?>
