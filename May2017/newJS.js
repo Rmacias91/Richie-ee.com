78e4beef3fff7ba6b036d8758780f75767ec157c
Richie <Rmacias2454@gmail.com>
2017-05-04 19:07:52 -0500

    Added Jquery and Mobile Friendly ViewPort

---
+++ May2017/newJS.js
@@ -0,0 +1,55 @@
+
+$(document).ready(function(){
+
+  //**About Me *************************************//
+  $("#closeBut1").click(function(){
+    $("#aboutMeDetail").slideUp(1000);
+  });
+  $("#minBut1").click(function(){
+    $("#aboutMeDetail").slideUp(1000);
+  });
+  $("#maxBut1").click(function(){
+    $("#aboutMeDetail").slideDown(1000);
+  });
+//**PROJECT 1 *************************************//
+  $("#closeBut2").click(function(){
+    $("#proj1").slideUp(1000);
+  });
+  $("#minBut2").click(function(){
+    $("#proj1").slideUp(1000);
+  });
+  $("#maxBut2").click(function(){
+    $("#proj1").slideDown(1000);
+  });
+//**PROJECT 2 *************************************//
+  $("#closeBut3").click(function(){
+    $("#proj2").slideUp(1000);
+  });
+  $("#minBut3").click(function(){
+    $("#proj2").slideUp(1000);
+  });
+  $("#maxBut3").click(function(){
+    $("#proj2").slideDown(1000);
+  });
+//**PROJECT 3 *************************************//
+  $("#closeBut4").click(function(){
+    $("#proj3").slideUp(1000);
+  });
+  $("#minBut4").click(function(){
+    $("#proj3").slideUp(1000);
+  });
+  $("#maxBut4").click(function(){
+    $("#proj3").slideDown(1000);
+  });
+//**PROJECT 4 *************************************//
+  $("#closeBut5").click(function(){
+    $("#proj4").slideUp(1000);
+  });
+  $("#minBut5").click(function(){
+    $("#proj4").slideUp(1000);
+  });
+  $("#maxBut5").click(function(){
+    $("#proj4").slideDown(1000);
+  });
+
+});
